725e472d20d175528997afab2f88aac87f4de310
YuraEnder <yuraender@yandex.ru>
2024-06-11 19:34:54 +0300

    Complete basic gRPC client

---
+++ gRPCClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using Grpc.Net.Client;
+using KOMPAS;
+using Microsoft.Extensions.Logging;
+
+namespace gRPCClient {
+
+    public class Program {
+
+        public static ILogger<Program>? Logger {
+            get; private set;
+        }
+        const int port = 5490;
+
+        public static void Main() {
+            var loggerFactory = LoggerFactory.Create(builder => {
+                builder.AddConsole();
+            });
+            Logger = loggerFactory.CreateLogger<Program>();
+
+            using (var channel = GrpcChannel.ForAddress("http://localhost:" + port)) {
+                var client = new Greeter.GreeterClient(channel);
+
+                var name = Console.ReadLine();
+                var answer = client.SayHello(new HelloRequest { Name = name });
+                Logger.LogInformation("Ответ сервера: {Answer}", answer.Message);
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
