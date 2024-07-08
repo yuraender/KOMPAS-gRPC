8a4ffe597bc6c4a3a3599cdea514743a98fc3ba7
YuraEnder <yuraender@yandex.ru>
2024-07-08 08:42:24 +0300

    Trying to create useful(?) request

--- WebServer/Controllers/HomeController.cs
+++ WebServer/Controllers/HomeController.cs
@@ -1,18 +1,29 @@
+﻿using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using Grpc.Net.Client;
 using KOMPAS;
+using Kompas6Constants;
 using Microsoft.AspNetCore.Mvc;
+using System.Text;
 using WebServer.Pages;
+using static KOMPAS.ObjectListResponse.Types.Object;
 
 namespace WebServer.Controllers {
 
     public class HomeController : Controller {
 
         public IActionResult Index() {
-            var response = KompasService.GetCacheData(
-                HttpContext.Connection.RemoteIpAddress.ToString(), "response");
-            if (response != null) {
-                TempData["response"] = response;
+            try {
+                using (var channel = GrpcChannel.ForAddress("http://127.0.0.1:5491")) {
+                    var client = new Kompas.KompasClient(channel);
+                    var isDocumentOpen = client.IsDocumentOpen(new Empty());
+                    if (isDocumentOpen != null) {
+                        TempData["status"] = isDocumentOpen.Value
+                            ? "Все готово к работе" : "Не открыт документ";
+                    }
+                }
+            } catch {
+                TempData["status"] = "Не запущен КОМПАС-3D";
             }
             return View();
         }
@@ -42,7 +53,162 @@ namespace WebServer.Controllers {
                 } catch (RpcException ex) {
                     TempData["response"] = ex.Status.Detail;
                 }
-            );
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult GetObjects() {
+            using (var channel = GrpcChannel.ForAddress("http://127.0.0.1:5491")) {
+                var client = new Kompas.KompasClient(channel);
+                try {
+                    var request = new ObjectListRequest();
+                    request.Types_.AddRange(new List<int> {
+                        (int) DrawingObjectTypeEnum.ksDrLDimension,
+                        (int) DrawingObjectTypeEnum.ksDrADimension,
+                        (int) DrawingObjectTypeEnum.ksDrDDimension,
+                        (int) DrawingObjectTypeEnum.ksDrRDimension,
+                        (int) DrawingObjectTypeEnum.ksDrRBreakDimension,
+                        (int) DrawingObjectTypeEnum.ksDrLBreakDimension,
+                        (int) DrawingObjectTypeEnum.ksDrABreakDimension,
+                        (int) DrawingObjectTypeEnum.ksDrOrdinateDimension,
+                        (int) DrawingObjectTypeEnum.ksDrArcDimension
+                    });
+                    var objects = client.GetObjects(request);
+                    if (objects != null) {
+                        StringBuilder sb = new StringBuilder();
+                        foreach (ObjectListResponse.Types.Object dObject in objects.Objects) {
+                            sb.AppendLine("============================");
+                            sb.AppendLine($"Name = {dObject.Name}");
+                            switch (dObject.DataCase) {
+                                case DataOneofCase.LineDimension:
+                                    var lineDim = dObject.LineDimension;
+                                    sb.AppendLine($"Angle = {lineDim.Angle}");
+                                    sb.AppendLine($"Orientation = {lineDim.Orientation}");
+                                    sb.AppendLine($"ShelfX = {lineDim.ShelfX}");
+                                    sb.AppendLine($"ShelfY = {lineDim.ShelfY}");
+                                    sb.AppendLine($"X1 = {lineDim.X1}");
+                                    sb.AppendLine($"X2 = {lineDim.X2}");
+                                    sb.AppendLine($"X3 = {lineDim.X3}");
+                                    sb.AppendLine($"Y1 = {lineDim.Y1}");
+                                    sb.AppendLine($"Y2 = {lineDim.Y2}");
+                                    sb.AppendLine($"Y3 = {lineDim.Y3}");
+                                    break;
+                                case DataOneofCase.AngleDimension:
+                                    var angleDim = dObject.AngleDimension;
+                                    sb.AppendLine($"Angle1 = {angleDim.Angle1}");
+                                    sb.AppendLine($"Angle2 = {angleDim.Angle2}");
+                                    sb.AppendLine($"DimensionType = {angleDim.DimensionType}");
+                                    sb.AppendLine($"Direction = {angleDim.Direction}");
+                                    sb.AppendLine($"Radius = {angleDim.Radius}");
+                                    sb.AppendLine($"ShelfX = {angleDim.ShelfX}");
+                                    sb.AppendLine($"ShelfY = {angleDim.ShelfY}");
+                                    sb.AppendLine($"XC = {angleDim.XC}");
+                                    sb.AppendLine($"X1 = {angleDim.X1}");
+                                    sb.AppendLine($"X2 = {angleDim.X2}");
+                                    sb.AppendLine($"X3 = {angleDim.X3}");
+                                    sb.AppendLine($"YC = {angleDim.YC}");
+                                    sb.AppendLine($"Y1 = {angleDim.Y1}");
+                                    sb.AppendLine($"Y2 = {angleDim.Y2}");
+                                    sb.AppendLine($"Y3 = {angleDim.Y3}");
+                                    break;
+                                case DataOneofCase.DiametralDimension:
+                                    var diametralDim = dObject.DiametralDimension;
+                                    sb.AppendLine($"Angle = {diametralDim.Angle}");
+                                    sb.AppendLine($"DimensionType = {diametralDim.DimensionType}");
+                                    sb.AppendLine($"Radius = {diametralDim.Radius}");
+                                    sb.AppendLine($"XC = {diametralDim.XC}");
+                                    sb.AppendLine($"YC = {diametralDim.YC}");
+                                    break;
+                                case DataOneofCase.RadialDimension:
+                                    var radialDim = dObject.RadialDimension;
+                                    sb.AppendLine($"Angle = {radialDim.Angle}");
+                                    sb.AppendLine($"BranchsCount = {radialDim.BranchsCount}");
+                                    sb.AppendLine($"DimensionType = {radialDim.DimensionType}");
+                                    sb.AppendLine($"Radius = {radialDim.Radius}");
+                                    sb.AppendLine($"ShelfX = {radialDim.ShelfX}");
+                                    sb.AppendLine($"ShelfY = {radialDim.ShelfY}");
+                                    sb.AppendLine($"XC = {radialDim.XC}");
+                                    sb.AppendLine($"YC = {radialDim.YC}");
+                                    break;
+                                case DataOneofCase.BreakRadialDimension:
+                                    var breakRadialDim = dObject.BreakRadialDimension;
+                                    sb.AppendLine($"Angle = {breakRadialDim.Angle}");
+                                    sb.AppendLine($"BreakAngle = {breakRadialDim.BreakAngle}");
+                                    sb.AppendLine($"BreakLength = {breakRadialDim.BreakLength}");
+                                    sb.AppendLine($"BreakX1 = {breakRadialDim.BreakX1}");
+                                    sb.AppendLine($"BreakX2 = {breakRadialDim.BreakX2}");
+                                    sb.AppendLine($"BreakY1 = {breakRadialDim.BreakY1}");
+                                    sb.AppendLine($"BreakY2 = {breakRadialDim.BreakY2}");
+                                    sb.AppendLine($"Radius = {breakRadialDim.Radius}");
+                                    sb.AppendLine($"TextOnLine = {breakRadialDim.TextOnLine}");
+                                    sb.AppendLine($"XC = {breakRadialDim.XC}");
+                                    sb.AppendLine($"YC = {breakRadialDim.YC}");
+                                    break;
+                                case DataOneofCase.BreakLineDimension:
+                                    var breakLineDim = dObject.BreakLineDimension;
+                                    sb.AppendLine($"ShelfX = {breakLineDim.ShelfX}");
+                                    sb.AppendLine($"ShelfY = {breakLineDim.ShelfY}");
+                                    sb.AppendLine($"X1 = {breakLineDim.X1}");
+                                    sb.AppendLine($"X2 = {breakLineDim.X2}");
+                                    sb.AppendLine($"X3 = {breakLineDim.X3}");
+                                    sb.AppendLine($"Y1 = {breakLineDim.Y1}");
+                                    sb.AppendLine($"Y2 = {breakLineDim.Y2}");
+                                    sb.AppendLine($"Y3 = {breakLineDim.Y3}");
+                                    break;
+                                case DataOneofCase.BreakAngleDimension:
+                                    var breakAngleDim = dObject.BreakAngleDimension;
+                                    sb.AppendLine($"Angle1 = {breakAngleDim.Angle1}");
+                                    sb.AppendLine($"Angle2 = {breakAngleDim.Angle2}");
+                                    sb.AppendLine($"DimensionType = {breakAngleDim.DimensionType}");
+                                    sb.AppendLine($"Direction = {breakAngleDim.Direction}");
+                                    sb.AppendLine($"Radius = {breakAngleDim.Radius}");
+                                    sb.AppendLine($"ShelfX = {breakAngleDim.ShelfX}");
+                                    sb.AppendLine($"ShelfY = {breakAngleDim.ShelfY}");
+                                    sb.AppendLine($"XC = {breakAngleDim.XC}");
+                                    sb.AppendLine($"X1 = {breakAngleDim.X1}");
+                                    sb.AppendLine($"X2 = {breakAngleDim.X2}");
+                                    sb.AppendLine($"X3 = {breakAngleDim.X3}");
+                                    sb.AppendLine($"YC = {breakAngleDim.YC}");
+                                    sb.AppendLine($"Y1 = {breakAngleDim.Y1}");
+                                    sb.AppendLine($"Y2 = {breakAngleDim.Y2}");
+                                    sb.AppendLine($"Y3 = {breakAngleDim.Y3}");
+                                    break;
+                                case DataOneofCase.HeightDimension:
+                                    var heightDim = dObject.HeightDimension;
+                                    sb.AppendLine($"DimensionType = {heightDim.DimensionType}");
+                                    sb.AppendLine($"X = {heightDim.X}");
+                                    sb.AppendLine($"X1 = {heightDim.X1}");
+                                    sb.AppendLine($"X2 = {heightDim.X2}");
+                                    sb.AppendLine($"Y = {heightDim.Y}");
+                                    sb.AppendLine($"Y1 = {heightDim.Y1}");
+                                    sb.AppendLine($"Y2 = {heightDim.Y2}");
+                                    break;
+                                case DataOneofCase.ArcDimension:
+                                    var arcDim = dObject.ArcDimension;
+                                    sb.AppendLine($"DimensionType = {arcDim.DimensionType}");
+                                    sb.AppendLine($"Direction = {arcDim.Direction}");
+                                    sb.AppendLine($"ShelfX = {arcDim.ShelfX}");
+                                    sb.AppendLine($"ShelfY = {arcDim.ShelfY}");
+                                    sb.AppendLine($"TextPointer = {arcDim.TextPointer}");
+                                    sb.AppendLine($"XC = {arcDim.XC}");
+                                    sb.AppendLine($"X1 = {arcDim.X1}");
+                                    sb.AppendLine($"X2 = {arcDim.X2}");
+                                    sb.AppendLine($"X3 = {arcDim.X3}");
+                                    sb.AppendLine($"YC = {arcDim.YC}");
+                                    sb.AppendLine($"Y1 = {arcDim.Y1}");
+                                    sb.AppendLine($"Y2 = {arcDim.Y2}");
+                                    sb.AppendLine($"Y3 = {arcDim.Y3}");
+                                    break;
+                            }
+                            sb.AppendLine("============================");
+                        }
+                        TempData["response"] = sb.ToString();
+                    }
+                } catch (RpcException ex) {
+                    TempData["response"] = ex.Status.Detail;
+                }
+            }
             return RedirectToAction("Index");
         }
     }
--- gRPCLibrary/Services/KompasService.cs
+++ gRPCLibrary/Services/KompasService.cs
@@ -1,5 +1,9 @@
+using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using KOMPAS;
+using Kompas6API5;
+using Kompas6Constants;
+using KompasAPI7;
 using System.Threading.Tasks;
 
 namespace gRPCLibrary.Services {
@@ -8,6 +12,136 @@ namespace gRPCLibrary.Services {
 
         public override Task<HelloResponse> SayHello(HelloRequest request, ServerCallContext context) {
             return Task.FromResult(new HelloResponse { Message = "Привет, " + request.Name });
+        }
+
+        public override Task<BoolValue> IsDocumentOpen(Empty request, ServerCallContext context) {
+            return Task.FromResult(new BoolValue { Value = Program.Application.ActiveDocument != null });
+        }
+
+        public override Task<ObjectListResponse> GetObjects(ObjectListRequest request, ServerCallContext context) {
+            ObjectListResponse response = new ObjectListResponse();
+            IKompasDocument document = Program.Application.ActiveDocument;
+            if (document == null || !(document is IKompasDocument2D)) {
+                return Task.FromResult(response);
+            }
+            ksDocument2D doc2D = Program.KompasAPI.ActiveDocument2D();
+            var dc = ((IKompasDocument2D) document).ViewsAndLayersManager.Views.ActiveView;
+            if (!(dc is IDrawingContainer container) || container.get_Objects(0) == null) {
+                return Task.FromResult(response);
+            }
+            foreach (IDrawingObject dObject in ((IDrawingContainer) dc).get_Objects(0)) {
+                if (!request.Types_.Contains((int) dObject.DrawingObjectType)) {
+                    continue;
+                }
+                var o = new ObjectListResponse.Types.Object {
+                    Name = doc2D.ksGetObjectNameByType((int) dObject.DrawingObjectType),
+                };
+                switch (dObject.DrawingObjectType) {
+                    case DrawingObjectTypeEnum.ksDrLDimension:
+                        ILineDimension ld = (ILineDimension) dObject;
+                        o.LineDimension = new ObjectListResponse.Types.Object.Types.LineDimension {
+                            Angle = ld.Angle,
+                            Orientation = (int) ld.Orientation,
+                            ShelfX = ld.ShelfX, ShelfY = ld.ShelfY,
+                            X1 = ld.X1, X2 = ld.X2, X3 = ld.X3,
+                            Y1 = ld.Y1, Y2 = ld.Y2, Y3 = ld.Y3
+                        };
+                        break;
+                    case DrawingObjectTypeEnum.ksDrADimension:
+                        IAngleDimension ad = (IAngleDimension) dObject;
+                        o.AngleDimension = new ObjectListResponse.Types.Object.Types.AngleDimension {
+                            Angle1 = ad.Angle1,
+                            Angle2 = ad.Angle2,
+                            DimensionType = (int) ad.DimensionType,
+                            Direction = ad.Direction,
+                            Radius = ad.Radius,
+                            ShelfX = ad.ShelfX, ShelfY = ad.ShelfY,
+                            X1 = ad.X1, X2 = ad.X2, X3 = ad.X3, XC = ad.Xc,
+                            Y1 = ad.Y1, Y2 = ad.Y2, Y3 = ad.Y3, YC = ad.Yc
+                        };
+                        break;
+                    case DrawingObjectTypeEnum.ksDrDDimension:
+                        IDiametralDimension dd = (IDiametralDimension) dObject;
+                        o.DiametralDimension = new ObjectListResponse.Types.Object.Types.DiametralDimension {
+                            Angle = dd.Angle,
+                            DimensionType = dd.DimensionType,
+                            Radius = dd.Radius,
+                            XC = dd.Xc, YC = dd.Yc
+                        };
+                        break;
+                    case DrawingObjectTypeEnum.ksDrRDimension:
+                        IRadialDimension rd = (IRadialDimension) dObject;
+                        o.RadialDimension = new ObjectListResponse.Types.Object.Types.RadialDimension {
+                            Angle = rd.Angle,
+                            BranchsCount = rd.BranchsCount,
+                            DimensionType = rd.DimensionType,
+                            Radius = rd.Radius,
+                            ShelfX = rd.ShelfX, ShelfY = rd.ShelfY,
+                            XC = rd.Xc, YC = rd.Yc
+                        };
+                        for (int i = 0; i < rd.BranchsCount; i++) {
+                            o.RadialDimension.BranchBegin.Add(rd.BranchBegin[i]);
+                        }
+                        break;
+                    case DrawingObjectTypeEnum.ksDrRBreakDimension:
+                        IBreakRadialDimension brd = (IBreakRadialDimension) dObject;
+                        o.BreakRadialDimension = new ObjectListResponse.Types.Object.Types.BreakRadialDimension {
+                            Angle = brd.Angle,
+                            BreakAngle = brd.BreakAngle,
+                            BreakLength = brd.BreakLength,
+                            BreakX1 = brd.BreakX1, BreakX2 = brd.BreakX2,
+                            BreakY1 = brd.BreakY1, BreakY2 = brd.BreakY2,
+                            Radius = brd.Radius,
+                            TextOnLine = (int) brd.TextOnLine,
+                            XC = brd.Xc, YC = brd.Yc
+                        };
+                        break;
+                    case DrawingObjectTypeEnum.ksDrLBreakDimension:
+                        IBreakLineDimension bld = (IBreakLineDimension) dObject;
+                        o.BreakLineDimension = new ObjectListResponse.Types.Object.Types.BreakLineDimension {
+                            ShelfX = bld.ShelfX, ShelfY = bld.ShelfY,
+                            X1 = bld.X1, X2 = bld.X2, X3 = bld.X3,
+                            Y1 = bld.Y1, Y2 = bld.Y2, Y3 = bld.Y3
+                        };
+                        break;
+                    case DrawingObjectTypeEnum.ksDrABreakDimension:
+                        IBreakAngleDimension bad = (IBreakAngleDimension) dObject;
+                        o.BreakAngleDimension = new ObjectListResponse.Types.Object.Types.BreakAngleDimension {
+                            Angle1 = bad.Angle1,
+                            Angle2 = bad.Angle2,
+                            DimensionType = (int) bad.DimensionType,
+                            Direction = bad.Direction,
+                            Radius = bad.Radius,
+                            ShelfX = bad.ShelfX, ShelfY = bad.ShelfY,
+                            X1 = bad.X1, X2 = bad.X2, X3 = bad.X3, XC = bad.Xc,
+                            Y1 = bad.Y1, Y2 = bad.Y2, Y3 = bad.Y3, YC = bad.Yc
+                        };
+                        break;
+                    case DrawingObjectTypeEnum.ksDrOrdinateDimension:
+                        IHeightDimension hd = (IHeightDimension) dObject;
+                        o.HeightDimension = new ObjectListResponse.Types.Object.Types.HeightDimension {
+                            DimensionType = (int) hd.DimensionType,
+                            X = hd.X, X1 = hd.X1, X2 = hd.X2,
+                            Y = hd.X, Y1 = hd.Y1, Y2 = hd.Y2,
+                        };
+                        break;
+                    case DrawingObjectTypeEnum.ksDrArcDimension:
+                        IArcDimension arcD = (IArcDimension) dObject;
+                        o.ArcDimension = new ObjectListResponse.Types.Object.Types.ArcDimension {
+                            DimensionType = arcD.DimensionType,
+                            Direction = arcD.Direction,
+                            ShelfX = arcD.ShelfX, ShelfY = arcD.ShelfY,
+                            TextPointer = arcD.TextPointer,
+                            X1 = arcD.X1, X2 = arcD.X2, X3 = arcD.X3, XC = arcD.Xc,
+                            Y1 = arcD.Y1, Y2 = arcD.Y2, Y3 = arcD.Y3, YC = arcD.Yc
+                        };
+                        break;
+                    default:
+                        break;
+                }
+                response.Objects.Add(o);
+            }
+            return Task.FromResult(response);
         }
     }
 }
